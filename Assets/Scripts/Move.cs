37867833455cc66341d1f208843793c1d5e3f873
DheboraCoelho <dheboracoelho@gmail.com>
2023-07-06 04:09:23 -0300

    personagem atualizado

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] CharacterController _controller;
+    [SerializeField] float _velocidade;
+    [SerializeField] float _moveH;
+    [SerializeField] float _moveZ;
+    [SerializeField] float _jumpForce;
+    [SerializeField] float _girarSpeed;
+    Vector3 _playerVelocity;
+    [SerializeField] float _forceGravity = -9.81f;
+    Animator _anim;
+    [SerializeField] float _speed;
+    float _speedAnimY;
+    [SerializeField] Rigidbody _rid;
+    [SerializeField] bool _isGrounded;
+    [SerializeField] float _rot;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _anim = GetComponent<Animator>();
+
+    }
+    void Andar()
+    {
+        _isGrounded = _controller.isGrounded;
+        _moveZ = Input.GetAxisRaw("Vertical");
+        _controller.Move(transform.forward * _moveZ * Time.deltaTime);
+        _speed = _moveZ;
+        _anim.SetFloat("correndo", _speed);
+        
+    }
+   void RoationPlayer()
+    {
+        _rot -= Input.GetAxis("Horizontal") * _girarSpeed;
+        transform.localEulerAngles=new Vector3(0.0f,_rot, 0.0f);
+
+    }
+    void GravityMode()
+    {
+        _playerVelocity.y= _forceGravity*Time.deltaTime;
+        _controller.Move(_playerVelocity*Time.deltaTime);
+    }
+    void Jump()
+    {
+        if ((Input.GetAxisRaw("Jump")>0 && _controller.isGrounded==true)){
+
+            _playerVelocity.y += Mathf.Sqrt(_jumpForce * -3.0f * _forceGravity);
+        }
+    }
+
+    void Update()
+    {
+        _anim.SetBool("chekground", _controller.isGrounded);
+        if (_controller.isGrounded== false) {
+            GravityMode();
+        }
+        _speedAnimY=_controller.velocity.y;
+        _anim.SetFloat("PulandoY", _speedAnimY);
+        Jump();
+        Andar();
+        GravityMode();
+        RoationPlayer();
+    }
+}
