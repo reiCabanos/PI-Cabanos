a9495e94338ba43b85a876f0f7af1ed80da05140
DheboraCoelho <130596424+DheboraCoelho@users.noreply.github.com>
2023-09-28 21:35:07 -0300

    configuracao de menu criada

--- Assets/Scripts/MenuControle.cs
+++ Assets/Scripts/MenuControle.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuControle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private string _nomeDaScena;
+    [SerializeField]GameObject _painelMenuInicial;
+    [SerializeField]GameObject _painelOpcoes;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+    public void Jogar()
+    {
+        SceneManager.LoadScene(_nomeDaScena);
+
+    }
+    public void AbrirOpcoes()
+    {
+        _painelMenuInicial.SetActive(false);
+        _painelOpcoes.SetActive(true);
+
+    }
+    public void FecharOpcoes()
+    {
+        _painelOpcoes.SetActive(false);
+        _painelMenuInicial.SetActive(true);
+    }
+    public void SairDoJogo()
+    {
+        Debug.Log("sair dom jogo");
+        Application.Quit();
+
+    }
+
+    
 }
