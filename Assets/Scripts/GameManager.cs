3250010e78f16a8107ef298976196714241229d7
Dhebora Coelho <dheboracoelho@gmail.com>
2024-04-22 17:38:19 -0300

    Seves

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] int _fase;
+    [SerializeField] int _partFase;
+    [SerializeField] int _life;
+    [SerializeField] Transform[] _pos;
+    [SerializeField] Transform _posPlayer;
+    void Start()
+    {
+
+        // acessar a memoria do computador
+        /* PlayerPrefs.SetInt("fase", 0);
+         _fase = PlayerPrefs.GetInt("fase");
+        */
+        Carregar();
+        _posPlayer.transform.localPosition = _pos[_partFase].transform.position;
+        
+    }
+
+    // Update is called once per frame
+    public void AumentarFase()
+    {
+        _fase++;
+    }
+    public void Salvar()
+    {
+        PlayerPrefs.SetInt("fase", _fase);
+    }
+    public void Carregar()
+    {
+        _fase = PlayerPrefs.GetInt("fase");
+    }
+    void Update()
+    {
+
+    }
+}
+
