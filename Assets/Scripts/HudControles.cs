170038d9f54db644a2fede10b69a6622e292d18a
DheboraCoelho <130596424+DheboraCoelho@users.noreply.github.com>
2024-05-15 12:44:53 -0300

    Ajustes Controles

--- Assets/Scripts/HudControles.cs
+++ Assets/Scripts/HudControles.cs
@@ -7,6 +7,7 @@ using Button = UnityEngine.UI.Button;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
 using DG.Tweening;
+using UnityEditor.ShaderGraph;
 
 public class HudControles : MonoBehaviour
 {
@@ -19,6 +20,8 @@ public class HudControles : MonoBehaviour
     public bool _ativarInventario;
     public bool _ativarCelular;
     public bool _sair;
+    public Transform _painelControles;
+    public Transform _painelBranco;
 
 
     void Start()
@@ -38,6 +41,7 @@ public class HudControles : MonoBehaviour
         {
             _sect.Select();
             _telaIniciar.DOScale(0, 0.5f);
+            StartCoroutine(PainelControles());
             _telaHuds.DOScale(1, 1f);
             Debug.Log("controles");
         }
@@ -50,7 +54,9 @@ public class HudControles : MonoBehaviour
         {
             
             _telaHuds.DOScale(0, 0.5f);
+            _telaCelular.DOScale(0, 0.5f);
             _telaInventario.DOScale(1, 1f);
+            _painelBranco.DOScale(1, 1f);
             Debug.Log("inventario");
         }
         
@@ -65,7 +71,9 @@ public class HudControles : MonoBehaviour
         if (_ativarCelular = value.performed)
         {
             _telaHuds.DOScale(0, 0.5f);
+            _telaInventario.DOScale(0, 0.5f);
             _telaCelular.DOScale(1, 1f);
+            _painelBranco.DOScale(1, 1f);
             Debug.Log("Celular");
         }
 
@@ -82,12 +90,22 @@ public class HudControles : MonoBehaviour
             _telaHuds.DOScale(1, 1f);
             _telaCelular.DOScale(0, 0.5f);
             _telaInventario.DOScale(0, 0.5f);
+            _painelBranco.DOScale(0, 0.5f);
             Debug.Log("sair");
         }
 
 
 
 
+
+    }
+
+
+    IEnumerator PainelControles()
+    {
+        _painelControles.DOScale(1, 1f);
+        yield return new WaitForSeconds(1.7f);
+        _painelControles.DOScale(0, 0.5f);
 
     }
 }
