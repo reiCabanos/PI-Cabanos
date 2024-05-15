2c69aa0a3ba6c63a8cfb1f7f54a2d86a362ec0fc
DheboraCoelho <130596424+DheboraCoelho@users.noreply.github.com>
2024-05-15 11:29:25 -0300

    configuração de controles

--- Assets/Scripts/HudControles.cs
+++ Assets/Scripts/HudControles.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Button = UnityEngine.UI.Button;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+using DG.Tweening;
 
 public class HudControles : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform _telaIniciar;
+    public Transform _telaHuds;
+    public Transform _telaCelular;
+    public Transform _telaInventario;
+    public bool _ativar;
+    public Button _sect;
+    public bool _ativarInventario;
+    public bool _ativarCelular;
+    public bool _sair;
+
+
     void Start()
     {
         
@@ -14,5 +30,64 @@ public class HudControles : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetComecar(InputAction.CallbackContext value)
+    {
+        if(_ativar = value.performed)
+        {
+            _sect.Select();
+            _telaIniciar.DOScale(0, 0.5f);
+            _telaHuds.DOScale(1, 1f);
+            Debug.Log("controles");
+        }
+      
+        
+    }
+    public void SetInventario(InputAction.CallbackContext value)
+    {
+        if (_ativarInventario = value.performed)
+        {
+            
+            _telaHuds.DOScale(0, 0.5f);
+            _telaInventario.DOScale(1, 1f);
+            Debug.Log("inventario");
+        }
+        
+            
+        
+
+
+    }
+
+    public void SetCelular(InputAction.CallbackContext value)
+    {
+        if (_ativarCelular = value.performed)
+        {
+            _telaHuds.DOScale(0, 0.5f);
+            _telaCelular.DOScale(1, 1f);
+            Debug.Log("Celular");
+        }
+
+
+
+
+
+    }
+
+    public void SetSair(InputAction.CallbackContext value)
+    {
+        if (_sair = value.performed)
+        {
+            _telaHuds.DOScale(1, 1f);
+            _telaCelular.DOScale(0, 0.5f);
+            _telaInventario.DOScale(0, 0.5f);
+            Debug.Log("sair");
+        }
+
+
+
+
+
     }
 }
