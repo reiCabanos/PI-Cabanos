a9c6c6b34ada6bda8be08d67c46a04bcc4a3f077
Dhebora Coelho <dhebora48243126@edu.pa.senac.br>
2024-06-10 16:06:30 -0300

    CAMERA AJUSTADA

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -10,10 +10,10 @@ public class SmoothFollow : MonoBehaviour
     void Update()
     {
         float Interpolation = speed * Time.deltaTime;
-        Vector3 position = transform.position;
+        Vector3 position = transform.localPosition;
 
         position.x = Mathf.Lerp(transform.position.x, Target.position.x, Interpolation);
-        position.y = Mathf.Lerp(transform.position.y, Target.position.y, Interpolation);
+        position.y = Mathf.Lerp(transform.position.y, Target.position.y+5, Interpolation);
         position.z = Mathf.Lerp(transform.position.z, Target.position.z, Interpolation);
 
         transform.position = position;
